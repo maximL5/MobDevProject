37a95c5c62ea580bc57fa4d98a84020f5c803019
JApitpit <Jianpitpit@gmail.com>
2025-03-02 22:52:58 -0700

    Deck Page initial

--- mobdev-project/pages/deck.tsx
+++ mobdev-project/pages/deck.tsx
@@ -1,32 +1,66 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 
 export function DeckScreen() {
-    return (
-      <View style={styles.container}>
-        <Text>Deck Screen</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.chooseText}>Choose a Deck</Text>
+      <View style={styles.cardContainer}>
+        <View style={styles.cardRow}>
+          <DeckButton />
+          <DeckButton />
+        </View>
+        <View style={styles.cardRow}>
+          <DeckButton />
+          <DeckButton />
+        </View>
       </View>
-    );
-  }
+    </View>
+  );
+}
 
+function DeckButton() {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <Image source={require('../assets/cardplaceholder.jpg')} style={styles.deck} />
+    </TouchableOpacity>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  
-    button: {
-      backgroundColor: '#808080',
-      padding: 20,
-      borderRadius: 10,
-      marginBottom: 20,
-    },
-  
-    buttonText: {
-      fontSize: 20,
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#808080',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  chooseText: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    fontSize: 57,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  cardContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cardRow: {
+    flexDirection: 'row',
+    gap: 10,
+    justifyContent: 'center',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#fff',
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    marginTop: 50,
+  },
+  deck: {
+    height: 180,
+    width: 120,
+    borderRadius: 10,
+  },
+});
