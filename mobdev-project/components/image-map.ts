e7b2a9d8fe7e257c8c90f832eda9942e139677d9
maximL5 <maxim.lopez@edu.sait.ca>
2025-04-04 20:28:29 -0600

    added all the cards

--- mobdev-project/components/image-map.ts
+++ mobdev-project/components/image-map.ts
@@ -3,7 +3,16 @@ import cardsList from '../res/cards.json';
 export const cardsData = cardsList.cards;
 
 export const imageMap: Record<string, any> = {
-    "rizzler.png": require("../assets/images/rizzler.png")
-    // Add all 12 images here
+    "rizzler.png": require("../assets/images/rizzler.png"),
+    "peaked.png": require("../assets/images/peaked.png"),
+    "anime.png": require("../assets/images/anime.png"),
+    "moo-deng.png": require("../assets/images/moo-deng.png"),
+    "morning.png": require("../assets/images/morning.png"),
+    "oiia.png": require("../assets/images/oiia.png"),
+    "street.png": require("../assets/images/street.png"),
+    "fortni.png": require("../assets/images/fortni.png"),
+    "smite.png": require("../assets/images/smite.png"),
+    "chill.png": require("../assets/images/chill.png"),
+    "giga.png": require("../assets/images/giga.png"),
+    "elite.png": require("../assets/images/elite.png")
 };
-  
